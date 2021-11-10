217d06f1fc267cbfd8167fca40c6336fc1d5f2e0
AER-S <89151307+AER-S@users.noreply.github.com>
2021-11-10 15:35:00 +0100

    Binding the crouching state to left control button and make the collider follow the shape of Ellen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Animator animator;
 
     private bool run;
+    private bool crouch;
+    
     // Start is called before the first frame update
     void Start()
     {
         run = false;
+        crouch = false;
     }
 
     // Update is called once per frame
@@ -18,13 +21,19 @@ public class PlayerController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         run = Input.GetKey(KeyCode.LeftShift);
+        crouch = Input.GetKey(KeyCode.LeftControl);
         Animate(horizontal);
     }
 
     void Animate(float _horizontal)
     {
-        transform.rotation = Quaternion.Euler(0f,90-Mathf.Sign(_horizontal)*90,0f);
+        
+        if (Mathf.Abs(_horizontal) > 0.2f)
+        {
+            transform.rotation = Quaternion.Euler(0f, 90 - Mathf.Sign(_horizontal) * 90, 0f);
+        }
         animator.SetFloat("speed",Mathf.Abs(_horizontal));
         animator.SetBool("run",run);
+        animator.SetBool("crouch",crouch);
     }
 }
