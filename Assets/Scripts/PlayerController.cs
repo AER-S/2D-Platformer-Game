bd1b2fd499d1e9125ab36bb0ba4da4c72e1350c3
AER-S <89151307+AER-S@users.noreply.github.com>
2021-11-10 14:26:30 +0100

    binding the animator parameters to the horizontal axis input and left shift button. Now Ellen change animations dpending on input she will walk if the horizontal axis buttons are pressed and run if shift button is holded and will flip depending on the horizontal button pressed.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+
+    private bool run;
     // Start is called before the first frame update
     void Start()
     {
-        
+        run = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        run = Input.GetKey(KeyCode.LeftShift);
+        Animate(horizontal);
+    }
+
+    void Animate(float _horizontal)
+    {
+        transform.rotation = Quaternion.Euler(0f,90-Mathf.Sign(_horizontal)*90,0f);
+        animator.SetFloat("speed",Mathf.Abs(_horizontal));
+        animator.SetBool("run",run);
     }
 }
