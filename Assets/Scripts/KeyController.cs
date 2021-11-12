b1a60b8216e830f953999bb35fa0606ce2ec750e
AER-S <89151307+AER-S@users.noreply.github.com>
2021-11-13 00:01:28 +0100

    Binding fadeout of the key to pick up event

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -5,14 +5,39 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
-    [SerializeField] private int keyValue;
+    [SerializeField] private int keyValue = 100;
+    private Animator animator;
+    private bool fadingOut;
+
+    private void Awake()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        fadingOut = false;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<PlayerController>())
         {
             PlayerController player = other.gameObject.GetComponent<PlayerController>();
             player.PickUp(keyValue);
-            Destroy(gameObject);
+            if (!fadingOut)
+            {
+                animator.SetTrigger("fadeOut");
+                fadingOut = true;
+            }
+
+            StartCoroutine("FadeOut");
         }
+    }
+
+    IEnumerator FadeOut()
+    {
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        Destroy(gameObject);
     }
 }
