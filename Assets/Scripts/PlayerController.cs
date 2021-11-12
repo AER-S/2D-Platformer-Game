7cb92a6d55e4062ae205ae3b5ef2e32bdd427f73
AER-S <89151307+AER-S@users.noreply.github.com>
2021-11-12 15:05:45 +0100

    I added the condition of being on the ground to call corouch and walk animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -45,8 +45,12 @@ public class PlayerController : MonoBehaviour
             jump = true;
         }
         
-        animator.SetFloat("speed",Mathf.Abs(_horizontal));
         animator.SetBool("run",run);
-        animator.SetBool("crouch",crouch);
+        
+        if (onGround)
+        {
+            animator.SetFloat("speed",Mathf.Abs(_horizontal));
+            animator.SetBool("crouch",crouch);
+        }
     }
 }
